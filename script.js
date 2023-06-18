5057c01b5e1a5ac09c0b5577cfc3b81aed9183c2
ENG AYUUB <moztaphem9@gmal.com>
2023-06-18 14:59:56 +0300

    Broken-keys done

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Good Luck 💪🏾
+function brokenKeys(Sax, khalad) {
+    let brokenKeys = [];
+    for (let i = 0; i < Sax.length; i++) {
+      if (Sax[i] !== khalad[i]) {
+        if (!brokenKeys.includes(Sax[i])) {
+          brokenKeys.push(Sax[i]);
+        }
+      }
+    }
+    return brokenKeys;
+  }
+  console.log(brokenKeys("fikrcamp bootcamp", "fikrcomp boorcamp")); // ["a","t"]
+  console.log(brokenKeys("good night", "gppd night")) // ["o"]
+
+
+
+
+
+
+
+
+  
